1b1ba26cc71e13f3871f4d1938cc529b815c1812
Mark Jones <mjones490@gmail.com>
2015-12-16 05:41:00 +0000

    Fixed timing issues
    git-svn-id: svn://localhost/repository/trunk/emulators@111 634a02ed-3b89-4193-ba82-24076204cd68

--- 6502_cpu/modes.c
+++ 6502_cpu/modes.c
@@ -33,17 +33,32 @@ BYTE absolute(enum ACCESS_TYPE access, BYTE value)
     return access_operand(access, value);
 }
 
+/**
+ * @brief Indicates when base address plus an offset is in a different page 
+ * space than the base address alone.
+ * @param[in] base Base address
+ * @param[in] offset Offset from base address
+ * @return True if offset is in a different page
+ */
+static inline WORD page_cross(WORD base, BYTE offset) 
+{
+    if (((base + offset) & 0xFF00) != (base & 0xFF00))
+        ++running.clocks;
+    return base + offset;
+}        
+
+
 BYTE absolute_X(enum ACCESS_TYPE access, BYTE value)
 {
     if (access != REPLACE) 
-       running.op_address = get_next_word() + regs.X; 
+       running.op_address = page_cross(get_next_word(), regs.X); 
     return access_operand(access, value);            
 }
 
 BYTE absolute_Y(enum ACCESS_TYPE access, BYTE value)
 {
     if (access != REPLACE) 
-       running.op_address = get_next_word() + regs.Y; 
+       running.op_address = page_cross(get_next_word(), regs.Y); 
     return access_operand(access, value);            
 }
 
@@ -71,7 +86,8 @@ BYTE zero_page_Y(enum ACCESS_TYPE access, BYTE value)
 BYTE zero_page_indirect_Y(enum ACCESS_TYPE access, BYTE value)
 {
     if (access != REPLACE)
-        running.op_address = get_word((WORD) get_next_byte()) + regs.Y;
+        running.op_address = page_cross(get_word((WORD) get_next_byte()),
+            regs.Y);
     return access_operand(access, value);
 }
 
