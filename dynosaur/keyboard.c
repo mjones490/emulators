a6897e710914334decde26920f111931ca1e1297
mjones490 <mjones490@gmail.com>
2020-03-26 23:17:42 -0500

    Added keyboard status port.

--- dynosaur/keyboard.c
+++ dynosaur/keyboard.c
@@ -15,21 +15,32 @@ BYTE keyboard_port(BYTE port, bool read, BYTE value)
     return value;
 }
 
+BYTE key_stat = 0;
+
+BYTE key_stat_port(BYTE port, bool read, BYTE value)
+{
+    return key_stat;
+}
+
 void check_keyboard()
 {
     SDL_Event e;
 
     while (SDL_PollEvent(&e) != 0) {
         if (e.type == SDL_TEXTINPUT) {
-            LOG_INF("Key = '%s'\n", e.text.text);
+            LOG_DBG("Key = '%s'\n", e.text.text);
             key_set[0] = e.text.text[0];
         } else if (e.type == SDL_KEYDOWN) {
-            LOG_INF("Key = 0x%02x\n", e.key.keysym.sym);
+            key_stat |= 0x01;
+            LOG_DBG("Key = 0x%02x\n", e.key.keysym.sym);
             if (e.key.keysym.sym > 255)
                 key_set[2] = e.key.keysym.sym & 0xff;
             else
                 key_set[1] = e.key.keysym.sym & 0xff;
+        } else if (e.type == SDL_KEYUP) {
+            key_stat &= 0xfe;
         }
+
     }
 }
 
@@ -40,6 +51,7 @@ void init_keyboard()
     attach_port(keyboard_port, 0x10);
     attach_port(keyboard_port, 0x11);
     attach_port(keyboard_port, 0x12);
+    attach_port(key_stat_port, 0x13);
 }
 
 void finalize_keyboard()
